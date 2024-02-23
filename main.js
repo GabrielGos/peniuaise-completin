8717465cc8f18e5f7720ddd65ccfa48828c34d97
GabrielGos <131292243+GabrielGos@users.noreply.github.com>
2024-02-23 14:20:32 -0300

    n tenho barquinho de papel

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX=0;
+noseY=0;
+//clownNose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+
+function preload() {
+  clownNose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+ 
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+  console.log("PoseNet is On");
+}
+
+
+function draw() {
+ image(video, 0, 0, 300, 300);
+ image(clownNose, noseX, noseY, 30, 30);
+}
+
+function takeSnapshot(){    
+  save('myFilterImage.png');
+}
+
+function gotPoses(results){
+if(results.length > 0)
+{
+
+console.log(results);
+noseX = results[0].pose.net.x
+noseY = results[0].pose.net.y
+console.log("nose x = " + results[0].pose.nose.x);
+console.log("nose y = " + results[0].pose.nose.y);
+}
+}
+
+function modelLoaded(){
+  console.log("PoseNet is On")
+}
+
+
